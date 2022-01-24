5344ef1da2f7bed10455d5f7d57a34d3a144c822
christios <christian-cayralat@hotmail.com>
2022-01-24 11:24:23 +0400

    Conjugation tables working for PV and IV MSA

---
+++ create_repr_verb_list.py
@@ -0,0 +1,53 @@
+import argparse
+
+from camel_tools.utils.charmap import CharMapper
+
+from db_maker import gen_cmplx_morph_combs, read_morph_specs, _generate_stem
+
+ar2bw = CharMapper.builtin_mapper('ar2bw')
+
+def create_representative_verbs_list(specs_sheets, config_file, config_name, cmplx_morph, output_name):
+    SHEETS, cond2class, _ = read_morph_specs(specs_sheets, config_file, config_name)
+    MORPH, LEXICON, HEADER = SHEETS['morph'], SHEETS['lexicon'], SHEETS['header']
+    _order = [line.split()[1:] for line in list(HEADER['Content']) if line.startswith('ORDER')][0]
+    _defaults = [{f: d for f, d in [f.split(':') for f in line.split()[1:]]}
+                 for line in list(HEADER['Content']) if line.startswith('DEFAULT')]
+    _defaults = {d['pos']: d for d in _defaults}
+    defaults = {'defaults': _defaults, 'order': _order}
+    defaults_ = defaults['defaults']['verb']
+    stem_feats = ['per', 'gen', 'num', 'mod', 'cas', 'enc0', 'prc0', 'prc1', 'prc2', 'prc3']
+    defaults_ = {sf: defaults_[sf] for sf in stem_feats}
+    defaults_['enc1'] = defaults_['enc0']
+
+    cmplx_stem_classes = gen_cmplx_morph_combs(
+        cmplx_morph, MORPH, LEXICON, cond2class)
+    
+    representative_verbs = []
+    for cmplx_stems in cmplx_stem_classes.values():
+        xconds = ' '.join([f['COND-S'] for f in cmplx_stems[0]])
+        xcondt = ' '.join([f['COND-T'] for f in cmplx_stems[0]])
+        xcondf = ' '.join([f['COND-F'] for f in cmplx_stems[0]])
+        info = _generate_stem(cmplx_stems[0], xconds, xcondt, xcondf, defaults_)
+        info = [feat.split(':')[1] for feat in info[0]['feats'].split() if feat.split(':')[0] in ['lex', 'diac']]
+        representative_verbs.append((ar2bw(info[1]), ar2bw(info[0]), xconds, xcondt))
+    with open(output_name, 'w') as f:
+        for info in representative_verbs:
+            print(*info, file=f, sep=',')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-specs_sheets", required=True,
+                        type=str, help="Excel spreadsheet containing all lexicon, morph, etc. sheets.")
+    parser.add_argument("-cmplx_morph", required=True,
+                        type=str, help="Complex stem/prefix/suffix class combination to generate the instances for.")
+    parser.add_argument("-config_file", required=True,
+                        type=str, help="Config file specifying which sheets to use from `specs_sheets`.")
+    parser.add_argument("-config_name", required=True,
+                        type=str, help="Name of the configuration to load from the config file.")
+    parser.add_argument("-output_name", required=True,
+                        type=str, help="Name of the file to output the representative lemmas to.")
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+
+    create_representative_verbs_list(
+        args.specs_sheets, args.config_file, args.config_name, args.cmplx_morph, args.output_name)
--- generate_conj_table.py
+++ generate_conj_table.py
@@ -1,23 +1,23 @@
-import sys
 import json
 import re
+from tqdm import tqdm
+import argparse
 
 from camel_tools.morphology.database import MorphologyDB
 from camel_tools.morphology.generator import Generator
-from camel_tools.morphology.analyzer import Analyzer
 from camel_tools.utils.charmap import CharMapper
 from camel_tools.morphology.utils import strip_lex
-
-from db_maker import gen_cmplx_morph_combs, read_morph_specs
 
 bw2ar = CharMapper.builtin_mapper('bw2ar')
 ar2bw = CharMapper.builtin_mapper('ar2bw')
 
-test_features = ['pos', 'asp', 'vox', 'per', 'gen', 'num',
+_test_features = ['pos', 'asp', 'vox', 'per', 'gen', 'num',
                  'mod', 'cas', 'enc0', 'prc0', 'prc1', 'prc2', 'prc3']
 # <POS>.<A><P><G><N>.<S><C><V><M>
 SIGNATURE_PATTERN = re.compile(
     r'([^\.]+)\.([^\.]{,4})\.([^\.]{,4})\.?P?(\d{,2})?\.?E?(\d{,2})?')
+ASPECT_PATTERN = re.compile(r'CV|IV|PV')
+
 sig2feat = {
     'feats0': {
         'pos': ['VERB']},
@@ -41,19 +41,6 @@ sig2feat = {
         'enc0': ['3ms_dobj']
     }
 }
-
-def create_representative_verbs_list():
-    SHEETS, cond2class, _ = read_morph_specs(sys.argv[1])
-    MORPH, LEXICON = SHEETS['morph'], SHEETS['lexicon']
-    cmplx_stem_classes = gen_cmplx_morph_combs(
-        '[IVPreBuff] [STEM-IV]', MORPH, LEXICON, cond2class)
-
-    representative_verbs = [
-        (c[0][0]['#LEMMA'], c[0][0]['FORM'], c[0][0]['COND-S'], c[0][0]['COND-T'])
-            for c in cmplx_stem_classes.values()]
-    with open('reprensentative_verbs_pv.csv', 'w') as f:
-        for info in representative_verbs:
-            print(*info, file=f, sep=',')
 
 def generate_feats(signature):
     match = SIGNATURE_PATTERN.search(signature)
@@ -85,68 +72,96 @@ def generate_feats(signature):
     return feats_
 
 def expand_paradigm(paradigms, aspect):
-    paradigm_ = paradigms['PV']['paradigm'][:]
-    if aspect == "PV":
-        if paradigms['PV']['passive']:
-            p_ = []
-            for signature in paradigms['PV']['paradigm']:
-                signature = re.sub('A', 'P', signature)
-                p_.append(signature)
-            paradigm_ += p_
-        else:
-            paradigm_ = paradigms['PV']['paradigm']
-        
-        if paradigms['PV']['enclitics']:
-            p_ = []
-            for signature in paradigms['PV']['paradigm']:
-                signature += '.E0'
-                p_.append(signature)
-            paradigm_ += p_
+    paradigm_ = paradigms[aspect]['paradigm'][:]
+    if paradigms[aspect]['passive']:
+        p_ = []
+        for signature in paradigms[aspect]['paradigm']:
+            signature = re.sub('A', 'P', signature)
+            p_.append(signature)
+        paradigm_ += p_
+    else:
+        paradigm_ = paradigms[aspect]['paradigm']
+    
+    if paradigms[aspect]['enclitics']:
+        p_ = []
+        for signature in paradigms[aspect]['paradigm']:
+            signature += '.E0'
+            p_.append(signature)
+        paradigm_ += p_
             
     return paradigm_
 
-db = MorphologyDB(sys.argv[2], flags='g')
-generator = Generator(db)
-
-db_camel = MorphologyDB(sys.argv[2])
-analyzer_camel = Analyzer(db_camel)
-
-with open('conjugation/paradigms.json') as f:
-    paradigms = json.load(f)
-
-def create_conjugation_tables(file_name, aspect):
-    with open(file_name) as f:
+def create_conjugation_tables(lemmas_file_name,
+                              paradigm_key,
+                              paradigms,
+                              output_file_name,
+                              generator):
+    with open(lemmas_file_name) as f:
         lemmas_conj = []
-        for info in f:
+        for info in tqdm(f.readlines()):
             lemma, form, cond_s, cond_t = info.strip().split(',')
             lemma = bw2ar(strip_lex(lemma))
             form = bw2ar(form)
-            paradigm = expand_paradigm(paradigms, aspect)
+            paradigm = expand_paradigm(paradigms, paradigm_key)
             paradigm_ = {}
             for signature in paradigm:
                 features = generate_feats(signature)
-                analyses = generator.generate(lemma, features)
-                paradigm_[signature] = {'analyses': analyses, 'debug': (ar2bw(form), cond_s, cond_t)}
+                # Using altered local copy of generator.py in camel_tools
+                analyses = generator.generate(lemma, features, debug=True)
+                prefix_cats = [a[1] for a in analyses]
+                stem_cats = [a[2] for a in analyses]
+                suffix_cats = [a[3] for a in analyses]
+                analyses = [a[0] for a in analyses]
+                paradigm_[signature] = {
+                    'analyses': analyses,
+                    'debug': (ar2bw(form), cond_s, cond_t, prefix_cats, stem_cats, suffix_cats)}
             lemmas_conj.append(paradigm_)
 
     conjugations = []
-    header = ["SIGNATURE", "LEMMA", "STEM", "COND-S", "COND-T", "FEATURES", "DIAC", "BW", "COUNT"]
+    header = ["SIGNATURE", "LEMMA", "STEM", "COND-S", "COND-T",
+              "PREFIX-CAT", "STEM-CAT", "SUFFIX-CAT", "FEATURES", "DIAC", "BW", "COUNT"]
 
     for paradigm in lemmas_conj:
         for signature, info in paradigm.items():
-            for analysis in info['analyses']:
+            for i, analysis in enumerate(info['analyses']):
                 signature = re.sub('Q', 'P', signature)
-                output = [signature, ar2bw(analysis['lex']), *info['debug']]
-                output.append(' '.join([f"{feat}:{analysis[feat]}" for feat in test_features]))
+                output = [signature, ar2bw(analysis['lex']), *info['debug'][:3]]
+                output += [info['debug'][3][i], info['debug'][4][i], info['debug'][5][i]]
+                output.append(' '.join(
+                    [f"{feat}:{analysis[feat]}" for feat in _test_features if feat in analysis]))
                 output += [ar2bw(analysis['diac']), ar2bw(analysis['bw']), str(len(info['analyses']))]
                 conjugations.append(output)
 
-    with open('conjugation/conjugations_pv_v2.0_clitics.tsv', 'w') as f:
+    with open(output_file_name, 'w') as f:
         print(*header, sep='\t', file=f)
         for output in conjugations:
             print(*output, sep='\t', file=f)
 
-create_representative_verbs_list()
-for aspect in ['PV', 'IV']:
-    file_name = f'representative_verbs_{aspect.lower()}.csv'
-    # create_conjugation_tables(file_name, aspect)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-paradigms", required=True,
+                        type=str, help="Configuration file containing the sets of paradigms from which we generate conjugation tables.")
+    parser.add_argument("-db", required=True,
+                        type=str, help="DB file which will be used with the generation module.")
+    parser.add_argument("-asp", required=True, choices=['p', 'i', 'c'],
+                        type=str, help="Aspect to generate the conjugation tables for.")
+    parser.add_argument("-mod", choices=['i', 's', 'j'],
+                        type=str, help="Aspect to generate the conjugation tables for.")
+    parser.add_argument("-repr_lemmas", required=True,
+                        type=str, help="Name of the file from which to load the representative lemmas from.")
+    parser.add_argument("-output_name", required=True,
+                        type=str, help="Name of the file to output the conjugation tables to.")
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+
+    db = MorphologyDB(args.db, flags='g')
+    generator = Generator(db)
+
+    with open(args.paradigms) as f:
+        paradigms = json.load(f)
+    asp = f"asp:{args.asp}"
+    mod = f" mod:{args.mod}" if args.asp == 'i' else ''
+    create_conjugation_tables(lemmas_file_name=args.repr_lemmas,
+                              paradigm_key=asp + mod,
+                              paradigms=paradigms,
+                              output_file_name=args.output_name,
+                              generator=generator)
