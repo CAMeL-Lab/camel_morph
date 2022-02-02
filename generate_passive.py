a6ab58c8164b204c2c81de1b586976646f2d0145
christios <christian-cayralat@hotmail.com>
2022-02-02 11:35:36 +0400

    Add generate_passive.py

---
+++ generate_passive.py
@@ -0,0 +1,119 @@
+import re
+import argparse
+import json
+import os
+from tqdm import tqdm
+
+import pandas as pd
+from numpy import nan
+from camel_tools.morphology.utils import strip_lex
+
+from utils import assign_pattern
+
+def generate_passive(LEXICON, patterns_path):
+    patterns = pd.read_csv(patterns_path)
+    patterns = patterns.replace(nan, '', regex=True)
+    patterns_map = {}
+    for _, row in patterns.iterrows():
+        info = dict(regex_match=row['REGEX-match'],
+                    regex_sub=row['REGEX-sub'],
+                    cond_map=row['COND-map'])
+        patterns_map[(row['Pattern'], row['COND-ST'])] = info
+
+    cond_t_pattern = re.compile(r'([cv]-suff)')
+    cond_s_pattern = re.compile(r'.* \+ (.*)')
+    one_or_more_pluses = re.compile(r' +')
+
+    def assign_pattern_wrapper(row):
+        pattern, _, _, _ = assign_pattern(strip_lex(row['LEMMA']))
+        return pattern if pattern else nan
+
+    def get_info(row):
+        info = patterns_map.get((row['PATTERN-DEF'], row['COND-TS']))
+        if info != None:
+            info['regex_sub'] = info['regex_sub'].replace('$', '\\')
+            return info
+        else:
+            return nan
+
+    def get_cond_t_pass(row):
+        match = cond_t_pattern.search(row['COND-TS'])
+        return match.group(1) if match else ''
+
+    def get_cond_s_pass(row):
+        match = cond_s_pattern.search(row['COND-TS'])
+        return match.group(1) if match else ''
+
+    
+    LEXICON_PASS = LEXICON.copy()
+    LEXICON_PASS['PATTERN-DEF'] = LEXICON_PASS.apply(assign_pattern_wrapper, axis=1)
+    LEXICON_PASS = LEXICON_PASS[LEXICON_PASS['PATTERN-DEF'].notna()]
+    LEXICON_PASS['COND-T'] = LEXICON_PASS['COND-T'].str.strip()
+    LEXICON_PASS['COND-T'] = LEXICON_PASS.apply(
+        lambda row: one_or_more_pluses.sub(' ', row['COND-T']), axis=1)
+    LEXICON_PASS['COND-S-NO-TRANS'] = LEXICON_PASS.apply(
+        lambda row: re.sub(r'\btrans|intrans', '', row['COND-S']), axis=1)
+    LEXICON_PASS['COND-S-NO-TRANS'] = LEXICON_PASS.apply(
+        lambda row: one_or_more_pluses.sub(' ', row['COND-S-NO-TRANS']), axis=1)
+    LEXICON_PASS['COND-S-NO-TRANS'] = LEXICON_PASS['COND-S-NO-TRANS'].str.strip()
+    LEXICON_PASS['COND-TS'] = LEXICON_PASS['COND-T']
+    LEXICON_PASS['COND-TS'] = LEXICON_PASS.apply(
+        lambda row: row['COND-TS'] + f"{' + ' if row['COND-T'] else ''}{row['COND-S-NO-TRANS']}", axis=1)
+    LEXICON_PASS['PATTERN-MAP'] = LEXICON_PASS.apply(get_info, axis=1)
+    LEXICON_PASS = LEXICON_PASS[LEXICON_PASS['PATTERN-MAP'].notna()]
+    LEXICON_PASS['FORM'] = LEXICON_PASS.apply(
+        lambda row: re.sub(row['PATTERN-MAP']['regex_match'],
+                           row['PATTERN-MAP']['regex_sub'],
+                           row['FORM']), axis=1)
+    LEXICON_PASS['COND-S'] = LEXICON_PASS['COND-S'].str.strip()
+    LEXICON_PASS['TRANS'] = LEXICON_PASS.apply(
+        lambda row: re.sub(r"\S+ +(intrans|trans)", r' \1', row['COND-S']), axis=1)
+    LEXICON_PASS['COND-TS'] = LEXICON_PASS.apply(
+        lambda row: row['PATTERN-MAP']['cond_map'] + row['TRANS'], axis=1)
+    LEXICON_PASS['COND-T'] = LEXICON_PASS.apply(get_cond_t_pass, axis=1)
+    LEXICON_PASS['COND-S'] = LEXICON_PASS.apply(get_cond_s_pass, axis=1)
+    LEXICON_PASS['BW'] = LEXICON_PASS.apply(
+        lambda row: re.sub(r'(.V)', r'\1_PASS', row['BW']), axis=1)
+    LEXICON_PASS['FEAT'] = LEXICON_PASS.apply(
+        lambda row: re.sub(r'asp:a', r'asp:p', row['FEAT']), axis=1)
+
+    LEXICON_PASS.drop('PATTERN-DEF', axis=1, inplace=True)
+    LEXICON_PASS.drop('COND-S-NO-TRANS', axis=1, inplace=True)
+    LEXICON_PASS.drop('COND-TS', axis=1, inplace=True)
+    LEXICON_PASS.drop('PATTERN-MAP', axis=1, inplace=True)
+    LEXICON_PASS.drop('TRANS', axis=1, inplace=True)
+
+    return LEXICON_PASS
+            
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-input_file", required=True,
+                        type=str, help="Path of active aspect lexicon to generate the passive from.")
+    parser.add_argument("-config_file", required=True,
+                        type=str, help="Config file specifying which sheets to use from `specs_sheets`.")
+    parser.add_argument("-config_name", required=True,
+                        type=str, help="Name of the configuration to load from the config file.")
+    parser.add_argument("-patterns", required=True,
+                        type=str, help="Path of file which contains the passive pattern maps.")
+    args = parser.parse_args()
+
+    with open(args.config_file) as f:
+        config = json.load(f)
+    data_dir = config['global']['data-dir']
+
+    LEXICON = pd.read_csv(os.path.join(data_dir, args.input_file))
+    # Replace spaces in BW and GLOSS with '#'; skip commented rows and empty lines
+    LEXICON = LEXICON[LEXICON.DEFINE == 'LEXICON']
+    LEXICON = LEXICON.replace(nan, '', regex=True)
+    LEXICON['GLOSS'] = LEXICON['GLOSS'].replace('\s+', '#', regex=True)
+    LEXICON['COND-S'] = LEXICON['COND-S'].replace(' +', ' ', regex=True)
+    LEXICON['COND-S'] = LEXICON['COND-S'].replace(' $', '', regex=True)
+    LEXICON['COND-T'] = LEXICON['COND-T'].replace(' +', ' ', regex=True)
+    LEXICON['COND-T'] = LEXICON['COND-T'].replace(' $', '', regex=True)
+
+    LEXICON_PASS = generate_passive(LEXICON, os.path.join(data_dir,args.patterns))
+    
+    output_name = re.sub(r'(.*).csv', r'\1-PASS.csv', args.input_file)
+    output_path = os.path.join(data_dir, output_name)
+    LEXICON_PASS.to_csv(output_path)
---
+++ utils.py
@@ -0,0 +1,182 @@
+import re
+
+def patternize_root(root):
+    """Will patternize denuded roots (except patterns which are inherently
+    geminate which it treats as a root), while keeping defective letters and
+    gemination apparent."""
+    pattern = []
+    soundness = []
+    c = 0
+
+    for char in root:
+        if char in [">", "&", "<", "}", "'"]:
+            pattern.append(">")
+            c += 1
+            soundness.append('mhmz')
+        elif char in ["w", "Y", "y"]:
+            pattern.append(char)
+            c += 1
+            soundness.append('def')
+        elif char in ["a", "i", "u", "o"]:
+            pattern.append(char)
+        elif char == "~":
+            pattern.append(char)
+            soundness.append('gem')
+        elif char == "A":
+            if c == 2:
+                pattern.append("aA")
+            else:
+                pattern.append("aAo")
+            c += 1
+            soundness.append('def')
+        else:
+            c += 1
+            pattern.append(str(c))
+
+    soundness_ = f"mhmz{soundness.count('mhmz')}+" if soundness.count(
+        'mhmz') else ''
+    soundness_ += f"def{soundness.count('def')}+" if soundness.count(
+        'def') else ''
+    soundness_ += f"gem+" if soundness.count('gem') else ''
+    soundness = "sound" if soundness_ == '' else soundness_[:-1]
+
+    return pattern, soundness
+
+
+def correct_soundness(soundness):
+    """For abstract patterns which are inherently geminate (e.g., 1a2~a3).
+    """
+    soundness = re.sub(r'\+{0,1}gem', '', soundness)
+    return soundness if soundness else 'sound'
+
+
+def assign_pattern(lemma):
+    lemma_undiac = re.sub(r'[auio]', '', lemma)
+    num_letters_lemma = len(lemma_undiac)
+    # Triliteral (denuded)
+    # 1a2a3
+    if num_letters_lemma == 3:
+        pattern, soundness = patternize_root(lemma)
+        abstract_pattern = "1a2a3"
+    # Triliteral (augmented) and quadriliteral (denuded and augmented)
+    elif num_letters_lemma > 3:
+        if num_letters_lemma == 4:
+            # 1a2~3 (tri)
+            if lemma[3] == "~" and lemma[1] != "A":
+                pattern, soundness = patternize_root(lemma)
+                soundness = correct_soundness(soundness)
+                abstract_pattern = "1a2~3"
+            # 1A2a3 (tri)
+            elif lemma[1] == "A":
+                lemma_ = lemma[:1] + lemma[2:]
+                pattern, soundness = patternize_root(lemma_)
+                pattern.insert(1, "A")
+                abstract_pattern = "1A2a3"
+            # >1o2a3 (tri) [has precedence over the next clause]
+            elif lemma[0] == ">":
+                lemma_ = lemma[2:]
+                pattern, soundness = patternize_root(lemma_)
+                pattern.insert(0, ">a")
+                abstract_pattern = ">1o2a3"
+            # 1a2o3a4 (quad)
+            elif lemma[3] == "o":
+                pattern, soundness = patternize_root(lemma)
+                abstract_pattern = "1a2o3a4"
+            else:
+                return None, None, None, '4'
+
+        elif num_letters_lemma == 5:
+            if lemma[0] == "t":
+                # ta1A2a3 (tri)
+                if lemma[3] == "A":
+                    lemma_ = lemma[2] + lemma[4:]
+                    pattern, soundness = patternize_root(lemma_)
+                    pattern.insert(0, "ta")
+                    pattern.insert(2, "A")
+                    abstract_pattern = "ta1A2a3"
+                # ta1a2~3 (tri) or ta1a2o3a4 (quad)
+                elif lemma[5] == "~" or lemma[5] == "o":
+                    lemma_ = lemma[2:]
+                    pattern, soundness = patternize_root(lemma_)
+                    soundness = correct_soundness(soundness)
+                    pattern.insert(0, "ta")
+                    abstract_pattern = "ta1a2~3" if lemma[5] == "~" else "ta1a2o3a4"
+                else:
+                    return None, None, None, '5+t'
+            # {ino1a2a3 (tri)
+            elif lemma.startswith("{ino"):
+                lemma_ = lemma[4:]
+                pattern, soundness = patternize_root(lemma_)
+                pattern.insert(0, "{ino")
+                abstract_pattern = "{ino1a2a3"
+            # {i1ota2a3 (tri)
+            elif lemma[0] == "{" and (lemma[4] in ["t", "T"] or lemma[3] == "~"):
+                abstract_pattern = "{i1ota2a3"
+                if len(lemma) == 7:
+                    lemma_ = lemma[2:4] + lemma[5:]
+                elif lemma[3] == "~":
+                    if len(lemma) == 6:
+                        lemma_ = lemma[2] + "o" + lemma[4:]
+                    else:
+                        lemma_ = lemma[2] + "o" + lemma[5:]
+                else:
+                    lemma_ = lemma[2:4] + lemma[6:]
+                pattern, soundness = patternize_root(lemma_)
+                pattern.insert(0, "{i")
+                if lemma[3] == "~":
+                    pattern[2] = "~"
+                    if len(lemma) != 6:
+                        pattern[2] = "~a"
+                elif len(lemma) in [6, 7]:
+                    pattern.insert(3, "t")
+                else:
+                    pattern.insert(3, "ta")
+            # {i1o2a3~ (tri)
+            elif lemma[0] == "{" and lemma[-1] == "~":
+                lemma_ = lemma[2:]
+                pattern, soundness = patternize_root(lemma_)
+                soundness = correct_soundness(soundness)
+                pattern.insert(0, "{i")
+                abstract_pattern = "{i1o2a3~"
+            else:
+                return None, None, None, '5'
+        elif num_letters_lemma == 6:
+            # {isota1o2a3 (tri)
+            if lemma.startswith("{iso"):
+                lemma_ = lemma[6:]
+                pattern, soundness = patternize_root(lemma_)
+                pattern.insert(0, "{isota")
+                abstract_pattern = "{isota1o2a3"
+            # {i1oEawo2a3 (tri)
+            elif lemma.startswith("{i") and lemma[6:8] == "wo":
+                lemma_ = lemma[2:4] + lemma[8:]
+                pattern, soundness = patternize_root(lemma_)
+                pattern.insert(0, "{i")
+                pattern.insert(3, "Eawo")
+                abstract_pattern = "{i1oEawo2a3"
+            # {i1o2a3a4~ (quad)
+            elif lemma[-1] == "~":
+                lemma_ = lemma[2:]
+                pattern, soundness = patternize_root(lemma_)
+                soundness = correct_soundness(soundness)
+                if soundness == "def1":
+                    pattern[3] = "aAo"
+                pattern.insert(0, "{i")
+                abstract_pattern = "{i1o2a3a4~"
+            # {i1o2ano3a4 (quad)
+            elif lemma[6:8] == "no":
+                lemma_ = lemma[2:6] + lemma[8:]
+                pattern, soundness = patternize_root(lemma_)
+                pattern.insert(0, "{i")
+                pattern.insert(5, "no")
+                abstract_pattern = "{i1o2ano3a4"
+            else:
+                return None, None, None, '6'
+        else:
+            return None, None, None, '>4'
+    # If there are less than 3 letters (maybe there is a problem)
+    else:
+        return None, None, None, '<3'
+
+    pattern = ''.join(pattern)
+    return pattern, abstract_pattern, soundness, None
