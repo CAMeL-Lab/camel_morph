5627ca27f4059412b1f00504af389869acd040d8
christios <christian-cayralat@hotmail.com>
2022-02-15 11:17:09 +0400

    Ignore soundness when generating passive

--- generate_passive.py
+++ generate_passive.py
@@ -13,18 +13,23 @@ from utils import assign_pattern
 errors = {}
 
 def generate_passive(LEXICON, patterns_path):
-    patterns = pd.read_csv(patterns_path)
-    patterns = patterns.replace(nan, '', regex=True)
-    patterns_map = {}
-    for _, row in patterns.iterrows():
+    passive_patterns = pd.read_csv(patterns_path)
+    passive_patterns = passive_patterns.replace(nan, '', regex=True)
+    passive_patterns['COND-ST'] = passive_patterns.apply(
+        lambda row: re.sub(r' ?\+? ?(gem|hamzated|hollow|defective) ?\+? ?', '', row['COND-ST']), axis=1)
+    passive_patterns['COND-map'] = passive_patterns.apply(
+        lambda row: re.sub(r' ?\+? ?(gem|hamzated|hollow|defective) ?\+? ?', '', row['COND-map']), axis=1)
+    passive_patterns_map = {}
+    for _, row in passive_patterns.iterrows():
         info = dict(regex_match=row['REGEX-match'],
                     regex_sub=row['REGEX-sub'],
                     cond_map=row['COND-map'])
-        patterns_map[(row['Pattern'], row['COND-ST'])] = info
+        passive_patterns_map[(row['Pattern'], row['COND-ST'])] = info
 
     cond_t_pattern = re.compile(r'([cv]-suff)')
     cond_s_pattern = re.compile(r'(?:.* \+ )?(.*)')
     one_or_more_pluses = re.compile(r' +')
+    soundness_pattern = re.compile(r'(hollow|defective|gem|hamzated)')
 
     def assign_pattern_wrapper(row):
         pattern, _, _, error = assign_pattern(strip_lex(row['LEMMA']))
@@ -33,7 +38,7 @@ def generate_passive(LEXICON, patterns_path):
         return pattern if pattern else nan
 
     def get_info(row):
-        info = patterns_map.get((row['PATTERN-DEF'], row['COND-TS']))
+        info = passive_patterns_map.get((row['PATTERN-DEF'], row['COND-TS']))
         if info != None:
             info['regex_sub'] = info['regex_sub'].replace('$', '\\')
             return info
@@ -47,7 +52,11 @@ def generate_passive(LEXICON, patterns_path):
     def get_cond_s_pass(row):
         match = cond_s_pattern.search(row['COND-TS'])
         return match.group(1) if match else ''
-    
+
+    def get_soundness(row):
+        match = soundness_pattern.search(row['COND-S'])
+        return match.group(1) if match else ''
+
     LEXICON_PASS = LEXICON.copy()
     LEXICON_PASS['PATTERN-DEF'] = LEXICON_PASS.apply(assign_pattern_wrapper, axis=1)
     LEXICON_PASS = LEXICON_PASS[LEXICON_PASS['PATTERN-DEF'].notna()]
@@ -55,13 +64,13 @@ def generate_passive(LEXICON, patterns_path):
     LEXICON_PASS['COND-T'] = LEXICON_PASS.apply(
         lambda row: one_or_more_pluses.sub(' ', row['COND-T']), axis=1)
     LEXICON_PASS['COND-S-NO-TRANS'] = LEXICON_PASS.apply(
-        lambda row: re.sub(r'\btrans|intrans', '', row['COND-S']), axis=1)
+        lambda row: re.sub(r' ?\+? ?(trans|intrans|gem|hamzated|hollow|defective) ?\+? ?', '', row['COND-S']), axis=1)
     LEXICON_PASS['COND-S-NO-TRANS'] = LEXICON_PASS.apply(
         lambda row: one_or_more_pluses.sub(' ', row['COND-S-NO-TRANS']), axis=1)
     LEXICON_PASS['COND-S-NO-TRANS'] = LEXICON_PASS['COND-S-NO-TRANS'].str.strip()
     LEXICON_PASS['COND-TS'] = LEXICON_PASS['COND-T']
     LEXICON_PASS['COND-TS'] = LEXICON_PASS.apply(
-        lambda row: row['COND-TS'] + f"{' + ' if row['COND-T'] else ''}{row['COND-S-NO-TRANS']}", axis=1)
+        lambda row: row['COND-TS'] + f"{' + ' if row['COND-TS'] and row['COND-S-NO-TRANS'] else ''}{row['COND-S-NO-TRANS']}", axis=1)
     LEXICON_PASS['PATTERN-MAP'] = LEXICON_PASS.apply(get_info, axis=1)
     LEXICON_PASS = LEXICON_PASS[LEXICON_PASS['PATTERN-MAP'].notna()]
     LEXICON_PASS['FORM'] = LEXICON_PASS.apply(
@@ -69,9 +78,10 @@ def generate_passive(LEXICON, patterns_path):
                            row['PATTERN-MAP']['regex_sub'],
                            row['FORM']), axis=1)
     LEXICON_PASS['COND-S'] = LEXICON_PASS['COND-S'].str.strip()
+    LEXICON_PASS['SOUND'] = LEXICON_PASS.apply(get_soundness, axis=1)
     # All passive forms should be intransitive
     LEXICON_PASS['COND-TS'] = LEXICON_PASS.apply(
-        lambda row: row['PATTERN-MAP']['cond_map'] + ' intrans', axis=1)
+        lambda row: row['PATTERN-MAP']['cond_map'] + (' ' if row['SOUND'] else '') + row['SOUND'] + ' intrans', axis=1)
     LEXICON_PASS['COND-T'] = LEXICON_PASS.apply(get_cond_t_pass, axis=1)
     LEXICON_PASS['COND-S'] = LEXICON_PASS.apply(get_cond_s_pass, axis=1)
     LEXICON_PASS['COND-S'] = LEXICON_PASS['COND-S'].str.strip()
@@ -85,6 +95,7 @@ def generate_passive(LEXICON, patterns_path):
     LEXICON_PASS.drop('COND-S-NO-TRANS', axis=1, inplace=True)
     LEXICON_PASS.drop('COND-TS', axis=1, inplace=True)
     LEXICON_PASS.drop('PATTERN-MAP', axis=1, inplace=True)
+    LEXICON_PASS.drop('SOUND', axis=1, inplace=True)
 
     return LEXICON_PASS
             
