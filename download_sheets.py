110a190bd4ff74c144d49287a30a3310f608b5d6
christios <christian-cayralat@hotmail.com>
2022-02-24 00:30:18 +0400

    Refactor download_sheets

--- download_sheets.py
+++ download_sheets.py
@@ -6,42 +6,29 @@ import time
 import gspread
 import pandas as pd
 
-sa = gspread.service_account(
-    "/Users/chriscay/.config/gspread/service_account.json")
+def download_sheets(lex, specs, save_dir, config_file, config_name):
+    sa = gspread.service_account(
+        "/Users/chriscay/.config/gspread/service_account.json")
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-lex", nargs='+', action='append', default=[],
-                        type=str, help="Name of the lexicon gsheets to download followed by the desired individual sheets.")
-    parser.add_argument("-specs", nargs='+', action='append', default=[],
-                        type=str, help="Name of the specs spreadsheet to download followed by the desired individual sheets.")
-    parser.add_argument("-save_dir", default="data",
-                        type=str, help="Path of the directory to save the csv files to.")
-    parser.add_argument("-config_file",
-                        type=str, help="Config file specifying which sheets to use.")
-    parser.add_argument("-config_name",
-                        type=str, help="Name of the configuration to load from the config file.")
-    args = parser.parse_args()
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
 
-    if not os.path.exists(args.save_dir):
-        os.mkdir(args.save_dir)
-    
-    if args.lex or args.specs:
-        lex = {'spreadsheets': [ss[0] for ss in args.lex],
-               'sheets': [ss[1:] for ss in args.lex]}
-        specs = {'spreadsheets': [ss[0] for ss in args.specs],
-                 'sheets': [ss[1:] for ss in args.specs]}
+    if lex or specs:
+        lex = {'spreadsheets': [ss[0] for ss in lex],
+               'sheets': [ss[1:] for ss in lex]}
+        specs = {'spreadsheets': [ss[0] for ss in specs],
+                 'sheets': [ss[1:] for ss in specs]}
     else:
-        with open(args.config_file) as f:
+        with open(config_file) as f:
             config = json.load(f)
         specs = {
             'spreadsheets': [config['global']['specs']['spreadsheet']],
             'sheets': [
-                config['global']['specs']['sheets'] + list(config['local'][args.config_name]['specs'].values())]
+                config['global']['specs']['sheets'] + list(config['local'][config_name]['specs'].values())]
         }
         lex = {
-            'spreadsheets': [config['local'][args.config_name]['lexicon']['spreadsheet']],
-            'sheets': [config['local'][args.config_name]['lexicon']['sheets']]
+            'spreadsheets': [config['local'][config_name]['lexicon']['spreadsheet']],
+            'sheets': [config['local'][config_name]['lexicon']['sheets']]
         }
 
     for spreadsheets in [lex, specs]:
@@ -62,6 +49,22 @@ if __name__ == "__main__":
                         else:
                             raise NotImplementedError
                 sheet = pd.DataFrame(sheet.get_all_records())
-                sheet.to_csv(os.path.join(args.save_dir, f"{sheet_name}.csv"))
+                sheet.to_csv(os.path.join(save_dir, f"{sheet_name}.csv"))
 
-    time.sleep(10)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-lex", nargs='+', action='append', default=[],
+                        type=str, help="Name of the lexicon gsheets to download followed by the desired individual sheets.")
+    parser.add_argument("-specs", nargs='+', action='append', default=[],
+                        type=str, help="Name of the specs spreadsheet to download followed by the desired individual sheets.")
+    parser.add_argument("-save_dir", default="data",
+                        type=str, help="Path of the directory to save the csv files to.")
+    parser.add_argument("-config_file",
+                        type=str, help="Config file specifying which sheets to use.")
+    parser.add_argument("-config_name",
+                        type=str, help="Name of the configuration to load from the config file.")
+    args = parser.parse_args()
+
+    download_sheets(args.lex, args.specs, args.save_dir,
+                    args.config_file, args.config_name)
