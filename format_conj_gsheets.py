8664434c8b9b72f82c77079b7ef6079567845f5c
christios <christian-cayralat@hotmail.com>
2022-03-18 10:51:13 +0400

    Fix paradigm debugging; add formatting and mode for upload

--- format_conj_gsheets.py
+++ format_conj_gsheets.py
@@ -18,8 +18,10 @@ if __name__ == "__main__":
                         type=str, help="Name of the spreadsheet to write the CSV file to (and to format).")
     parser.add_argument("-gsheet_name", required=True,
                         type=str, help="Name of the gsheet to write the CSV file to (and to format).")
-    parser.add_argument("-overwrite", default=False,
-                        action='store_true', help="Overwrite sheet content if sheet with same name already exists.")
+    parser.add_argument("-formatting", default='', choices=['conj_tables', 'bank'],
+                        type=str, help="How to format the sheet.")
+    parser.add_argument("-mode", default='prompt', choices=['backup', 'overwrite', 'prompt'],
+                        type=str, help="Mode that decides what to do if sheet which is being uploaded already exists.")
     args = parser.parse_args()
 
     sheet_csv = pd.read_csv(os.path.join(args.dir, args.file_name), sep='\t')
@@ -31,41 +33,61 @@ if __name__ == "__main__":
         worksheet = sh.add_worksheet(title=args.gsheet_name, rows="100", cols="20")
     except gspread.exceptions.APIError as e:
         if re.search(r'name.*already exists', e.args[0]['message']):
-            if not args.overwrite:
+            if args.mode == 'prompt':
                 response = input('A sheet with this name already exists. Do you still want to overwrite it? [y/n]: ')
-            else:
+            elif args.mode == 'overwrite':
                 response = 'y'
+            elif args.mode == 'backup':
+                response = 'b'
+                gsheet_name_bu = args.gsheet_name + '-Backup'
+
             if response == 'y':
                 worksheet = sh.worksheet(title=args.gsheet_name)
                 worksheet.clear()
                 print('Sheet content will be overwritten.')
+            elif response == 'b':
+                print(f'Previous sheet will be backed up under the name {gsheet_name_bu} (effectively overwriting the old backup sheet).')
+                if gsheet_name_bu in [sheet.title for sheet in sh.worksheets()]:
+                    worksheet_bu = sh.worksheet(title=gsheet_name_bu)
+                    sh.del_worksheet(worksheet_bu)
+                worksheet = sh.worksheet(title=args.gsheet_name)
+                sh.duplicate_sheet(source_sheet_id=worksheet.id,
+                                   insert_sheet_index=worksheet.index + 1,
+                                   new_sheet_name=gsheet_name_bu)
+                worksheet.clear()
             elif response == 'n':
                 print('Process aborted.')
                 sys.exit()
         else:
             raise NotImplementedError
+    
     worksheet.update(
         [sheet_csv.columns.values.tolist()] + sheet_csv.values.tolist())
-    rule1 = gspread_formatting.ConditionalFormatRule(
-        ranges=[gspread_formatting.GridRange.from_a1_range('A:S', worksheet)],
-        booleanRule=gspread_formatting.BooleanRule(
-            condition=gspread_formatting.BooleanCondition('CUSTOM_FORMULA', ['=$U1=0']),
-            format=gspread_formatting.CellFormat(
-                backgroundColor=gspread_formatting.Color(217/255, 234/255, 211/255))
-        )
-    )
-    rule2 = gspread_formatting.ConditionalFormatRule(
-        ranges=[gspread_formatting.GridRange.from_a1_range('A:S', worksheet)],
-        booleanRule=gspread_formatting.BooleanRule(
-            condition=gspread_formatting.BooleanCondition('CUSTOM_FORMULA', ['=$U1=1']),
-            format=gspread_formatting.CellFormat(
-                backgroundColor=gspread_formatting.Color(255/255, 242/255, 204/255))
-        )
-    )
-    rules = gspread_formatting.get_conditional_format_rules(worksheet)
-    rules.clear()
-    rules.append(rule1)
-    rules.append(rule2)
-    rules.save()
 
-    worksheet.freeze(rows=1, cols=9)
+    if args.formatting == 'conj_tables':
+        rule1 = gspread_formatting.ConditionalFormatRule(
+            ranges=[gspread_formatting.GridRange.from_a1_range('A:S', worksheet)],
+            booleanRule=gspread_formatting.BooleanRule(
+                condition=gspread_formatting.BooleanCondition('CUSTOM_FORMULA', ['=$U1=0']),
+                format=gspread_formatting.CellFormat(
+                    backgroundColor=gspread_formatting.Color(217/255, 234/255, 211/255))
+            )
+        )
+        rule2 = gspread_formatting.ConditionalFormatRule(
+            ranges=[gspread_formatting.GridRange.from_a1_range('A:S', worksheet)],
+            booleanRule=gspread_formatting.BooleanRule(
+                condition=gspread_formatting.BooleanCondition('CUSTOM_FORMULA', ['=$U1=1']),
+                format=gspread_formatting.CellFormat(
+                    backgroundColor=gspread_formatting.Color(255/255, 242/255, 204/255))
+            )
+        )
+        rules = gspread_formatting.get_conditional_format_rules(worksheet)
+        rules.clear()
+        rules.append(rule1)
+        rules.append(rule2)
+        rules.save()
+
+        worksheet.freeze(rows=1, cols=10)
+
+    else:
+        worksheet.freeze(rows=1)
--- paradigm_debugging.py
+++ paradigm_debugging.py
@@ -7,25 +7,31 @@ import gspread
 import pandas as pd
 from numpy import nan
 
+from camel_tools.morphology.utils import strip_lex
+
 header = ["line", "status", "count", "signature", "lemma", "diac_ar", "diac", "freq",
-          "qc", "pattern", "stem", "bw", "gloss", "cond-s", "cond-t", "pref-cat",
+          "qc", "comments", "pattern", "stem", "bw", "gloss", "cond-s", "cond-t", "pref-cat",
           "stem-cat", "suff-cat", "feats", "debug", "color"]
 
 class AnnotationBank:
     UNKOWN = 'UNK'
     GOOD = 'OK'
-    CORRECTION = 'CORRECT'
     PROBLEM = 'PROB'
-    TAGS = {UNKOWN, GOOD, CORRECTION, PROBLEM}
+    TAGS = {UNKOWN, GOOD, PROBLEM}
 
-    def __init__(self, bank_path, annotated_paradigms):
+    def __init__(self, bank_path, annotated_paradigms=None):
         self._bank_path = bank_path
         self._bank = OrderedDict()
 
         if os.path.exists(bank_path):
             self._read_bank()
 
-        self._update_bank(annotated_paradigms)
+        if annotated_paradigms is not None:
+            self._update_bank(annotated_paradigms)
+
+    def get(self, key, default=None):
+        return self._bank[key] if key in self._bank else default
+        
 
     def __contains__(self, key):
         return key in self._bank
@@ -39,46 +45,66 @@ class AnnotationBank:
     def _update_bank(self, annotated_paradigms):
         annotated_paradigms['QC'] = annotated_paradigms['QC'].replace(
             '', AnnotationBank.UNKOWN, regex=True)
+        annotated_paradigms['QC'] = annotated_paradigms['QC'].str.strip()
+        annotated_paradigms['QC'] = annotated_paradigms['QC'].str.upper()
+        annotated_paradigms['LEMMA'] = annotated_paradigms['LEMMA'].replace(r'_\d', '', regex=True)
         assert all([qc in AnnotationBank.TAGS for qc in annotated_paradigms['QC'].values.tolist()]), \
             f"Get rid of all tags not belonging to {AnnotationBank.TAGS}"
+        assert set(['SIGNATURE', 'LEMMA', 'DIAC', 'QC', 'COMMENTS']).issubset(annotated_paradigms.columns)
         
         for _, row in annotated_paradigms.iterrows():
-            key = (row['SIGNATURE'], row['LEMMA'], row['DIAC'], row['QC'])
-            if key not in self._bank and row['QC'] != AnnotationBank.UNKOWN:
-                self._bank[key] = 1
-            elif row['QC'] == AnnotationBank.CORRECTION:
-                for current_key in self._bank:
-                    if current_key[:-1] == key[:-1]:
-                        del self._bank[current_key]
-                        self._bank[key] = 1
+            key = (row['SIGNATURE'], row['LEMMA'], row['DIAC'])
+            if row['QC'] != AnnotationBank.UNKOWN:
+                if key not in self._bank:
+                    for k in self._bank:
+                        if key[:-1] == k[:-1]:
+                            self._bank[k]['QC'] = row['QC']
+                self._bank[key] = {'QC': row['QC'],
+                                    'COMMENTS': row.get('COMMENTS', '')}
+
         self._save_bank()
 
     def _save_bank(self):
         with open(self._bank_path, 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerows([[k for k in key] for key in self._bank])
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerow(['SIGNATURE', 'LEMMA', 'DIAC', 'QC', 'COMMENTS'])
+            writer.writerows([[k for k in key] + [self._bank[key][h] for h in ['QC', 'COMMENTS']]
+                                for key in self._bank])
 
     def _read_bank(self):
-        with open(self._bank_path) as f:
-            csvreader = csv.reader(f)
-            self._bank = OrderedDict((tuple(row), 1) for row in csvreader)
+        bank = pd.read_csv(self._bank_path, delimiter='\t')
+        bank = bank.replace(nan, '', regex=True)
+        bank['LEMMA'] = bank['LEMMA'].replace(r'_\d', '', regex=True)
+        self._bank = OrderedDict(
+            ((row['SIGNATURE'], row['LEMMA'], row['DIAC']), 
+                {'QC': row['QC'], 'COMMENTS': row.get('COMMENTS', '')})
+            for _, row in bank.iterrows())
 
-def automatic_bank_annotation(bank_path, annotated_paradigms):
+def automatic_bank_annotation(bank_path, annotated_paradigms, new_conj_tables):
     bank = AnnotationBank(bank_path, annotated_paradigms)
+    lemmas = [entry[1] for entry in bank._bank]
+    strip = True if not any(['-' in lemma for lemma in lemmas]) else False
 
     outputs = []
-    for _, row in annotated_paradigms.iterrows():
-        key = (row['SIGNATURE'], row['LEMMA'], row['DIAC'], row['QC'])
+    for _, row in new_conj_tables.iterrows():
+        lemma = row['LEMMA'] if not strip else strip_lex(row['LEMMA'])
+        key = (row['SIGNATURE'], lemma, row['DIAC'])
         row = row.to_dict()
-        if key in bank:
-            row['QC'] = bank[key]
-        #TODO: add elifs here
+        info = bank.get(key)
+        if info is not None:
+            row['QC'] = info['QC']
+            row['COMMENTS'] = info['COMMENTS']
         else:
-            row['QC'] = AnnotationBank.UNKOWN
+            for k, info in bank._bank.items():
+                if key[:-1] == k[:-1]:
+                    row['QC'] = f"({k[-1]})[{info['QC']}]>({row['DIAC']})[{AnnotationBank.UNKOWN}]"
+                    break
+            else:
+                row['QC'] = AnnotationBank.UNKOWN
 
         output_ordered = OrderedDict()
-        for h in header:
-            output_ordered[h.upper()] = row.get(h.upper(), '')
+        for k in header:
+            output_ordered[k.upper()] = row.get(k.upper(), '')
         
         outputs.append(output_ordered)
 
@@ -88,14 +114,16 @@ def automatic_bank_annotation(bank_path, annotated_paradigms):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-gsheet", default='',
-                        type=str, help="Name of the manually annotated paradigms gsheet.")
+                        type=str, help="Name of the manually annotated paradigms gsheet, the annotations of which will go in the bank.")
     parser.add_argument("-spreadsheet", default='',
                         type=str, help="Name of the spreadsheet in which that sheet is.")
     parser.add_argument("-use_local",  default='',
-                        type=str, help="Use local sheet instead of downloading (for testing). Add the name of the directory in which the sheet is as an argument.")
+                        type=str, help="Use local sheet instead of downloading. Add the name of the conjugation table path to use.")
+    parser.add_argument("-new_conj",  default='',
+                        type=str, help="Path of the conjugation tables file generated after fixes were made to the specification sheets. This file will be automatically annotated using the information in the bank.")
     parser.add_argument("-bank_dir",  default='conjugation_local/banks',
                         type=str, help="Directory in which the annotation banks are.")
-    parser.add_argument("-bank_name",  default='conjugation_local/paradigm_debugging',
+    parser.add_argument("-bank_name",  default='',
                         type=str, help="Name of the annotation bank to use.")
     parser.add_argument("-output_name",
                         type=str, help="Name of the file to output the automatically bank-annotated paradigm tables to.")
@@ -121,15 +149,20 @@ if __name__ == "__main__":
     sh = sa.open(args.spreadsheet)
 
     if args.use_local:
-        annotated_paradigms = pd.read_csv(os.path.join(args.use_local, args.gsheet))
+        print('Using local sheet...')
+        annotated_paradigms = pd.read_csv(args.use_local, delimiter='\t')
         annotated_paradigms = annotated_paradigms.replace(nan, '', regex=True)
     else:
         worksheet = sh.worksheet(title=args.gsheet)
         annotated_paradigms = pd.DataFrame(worksheet.get_all_records())
         annotated_paradigms.to_csv(os.path.join(args.output_dir, args.output_name))
 
-    outputs = automatic_bank_annotation(bank_path=os.path.join(args.bank_dir, f"{args.bank_name}.csv"),
-                                        annotated_paradigms=annotated_paradigms)
+    new_conj_tables = pd.read_csv(args.new_conj, delimiter='\t')
+    new_conj_tables = new_conj_tables.replace(nan, '', regex=True)
+
+    outputs = automatic_bank_annotation(bank_path=os.path.join(args.bank_dir, f"{args.bank_name}.tsv"),
+                                        annotated_paradigms=annotated_paradigms,
+                                        new_conj_tables=new_conj_tables)
 
     output_path = os.path.join(args.output_dir, args.output_name)
     with open(output_path, 'w') as f:
