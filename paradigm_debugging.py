725b804230ab4ed68e0506e82e3bd5043973065b
christios <christian-cayralat@hotmail.com>
2022-03-21 11:11:08 +0400

    Implement bank cleanup

--- paradigm_debugging.py
+++ paradigm_debugging.py
@@ -2,6 +2,7 @@ import argparse
 from collections import OrderedDict
 import os
 import csv
+import sys
 
 import gspread
 import pandas as pd
@@ -18,13 +19,27 @@ class AnnotationBank:
     GOOD = 'OK'
     PROBLEM = 'PROB'
     TAGS = {UNKOWN, GOOD, PROBLEM}
+    HEADER_KEY = ['SIGNATURE', 'LEMMA', 'DIAC']
+    HEADER_INFO = ['QC', 'COMMENTS', 'STATUS']
+    HEADER = HEADER_KEY + HEADER_INFO
 
-    def __init__(self, bank_path, annotated_paradigms=None):
+    def __init__(self, bank_path, annotated_paradigms=None, gsheet_info=None, download_bank=False):
         self._bank_path = bank_path
+        if gsheet_info is not None:
+            self._gsheet_name = gsheet_info['gsheet_name']
+            self._spreadsheet = gsheet_info['spreadsheet']
         self._bank = OrderedDict()
 
-        if os.path.exists(bank_path):
-            self._read_bank()
+        if not download_bank:
+            if os.path.exists(bank_path):
+                self._read_bank_from_tsv()
+        else:
+            sa = gspread.service_account(
+                "/Users/chriscay/.config/gspread/service_account.json")
+            sh = sa.open(self._spreadsheet)
+            worksheet = sh.worksheet(title=self._gsheet_name)
+            bank = pd.DataFrame(worksheet.get_all_records())
+            self._read_bank_from_df(bank)
 
         if annotated_paradigms is not None:
             self._update_bank(annotated_paradigms)
@@ -50,35 +65,69 @@ class AnnotationBank:
         annotated_paradigms['LEMMA'] = annotated_paradigms['LEMMA'].replace(r'_\d', '', regex=True)
         assert all([qc in AnnotationBank.TAGS for qc in annotated_paradigms['QC'].values.tolist()]), \
             f"Get rid of all tags not belonging to {AnnotationBank.TAGS}"
-        assert set(['SIGNATURE', 'LEMMA', 'DIAC', 'QC', 'COMMENTS']).issubset(annotated_paradigms.columns)
+        assert set(AnnotationBank.HEADER).issubset(annotated_paradigms.columns)
+
+        lemmas = [entry[1] for entry in self._bank]
+        strip = True if not any(['-' in lemma for lemma in lemmas]) else False
+        if strip:
+            annotated_paradigms['LEMMA'] = annotated_paradigms['LEMMA'].replace(r'-.', '', regex=True)
         
+        keys_to_del = []
         for _, row in annotated_paradigms.iterrows():
-            key = (row['SIGNATURE'], row['LEMMA'], row['DIAC'])
+            key_annot = tuple([row[h] for h in AnnotationBank.HEADER_KEY])
             if row['QC'] != AnnotationBank.UNKOWN:
-                if key not in self._bank:
-                    for k in self._bank:
-                        if key[:-1] == k[:-1]:
-                            self._bank[k]['QC'] = row['QC']
-                self._bank[key] = {'QC': row['QC'],
-                                    'COMMENTS': row.get('COMMENTS', '')}
+                if key_annot not in self._bank:
+                    for key_bank in self._bank:
+                        if key_annot[:-1] == key_bank[:-1]:
+                            # Correction case (give precedence to newer annotations)
+                            if row['QC'] == self._bank[key_bank]['QC'] == AnnotationBank.GOOD:
+                                keys_to_del.append(key_bank)
+                    for key_bank in keys_to_del:
+                        if key_bank in self._bank:
+                            del self._bank[key_bank]
+                self._bank[key_annot] = {h: row.get(h, '') for h in [h for h in AnnotationBank.HEADER_INFO if h != 'STATUS']}
+                self._bank[key_annot]['STATUS'] = ''
 
         self._save_bank()
 
     def _save_bank(self):
         with open(self._bank_path, 'w', newline='') as f:
             writer = csv.writer(f, delimiter='\t')
-            writer.writerow(['SIGNATURE', 'LEMMA', 'DIAC', 'QC', 'COMMENTS'])
-            writer.writerows([[k for k in key] + [self._bank[key][h] for h in ['QC', 'COMMENTS']]
+            writer.writerow(AnnotationBank.HEADER)
+            writer.writerows([[k for k in key] + [self._bank[key].get(h, '') for h in AnnotationBank.HEADER_INFO]
                                 for key in self._bank])
 
-    def _read_bank(self):
+    def _upload_gsheet(self, df=None):
+        sa = gspread.service_account(
+            "/Users/chriscay/.config/gspread/service_account.json")
+        sh = sa.open(self._spreadsheet)
+        worksheet = sh.worksheet(title=self._gsheet_name)
+        worksheet.clear()
+        bank = df if df is not None else self.to_df()
+        worksheet.update(
+            [df.columns.values.tolist()] + df.values.tolist())
+
+    def _read_bank_from_tsv(self):
         bank = pd.read_csv(self._bank_path, delimiter='\t')
         bank = bank.replace(nan, '', regex=True)
         bank['LEMMA'] = bank['LEMMA'].replace(r'_\d', '', regex=True)
         self._bank = OrderedDict(
-            ((row['SIGNATURE'], row['LEMMA'], row['DIAC']), 
-                {'QC': row['QC'], 'COMMENTS': row.get('COMMENTS', '')})
+            (tuple([row[h] for h in AnnotationBank.HEADER_KEY]), 
+                {h: row.get(h, '') for h in AnnotationBank.HEADER_INFO})
             for _, row in bank.iterrows())
+
+    def _read_bank_from_df(self, df):
+        if 'STATUS' in df.columns:
+            df = df[df['STATUS'] != 'DELETE']
+        for _, row in df.iterrows():
+            info = {h: row.get(h, '') for h in AnnotationBank.HEADER_INFO}
+            self._bank[tuple([row[h] for h in AnnotationBank.HEADER_KEY])] = info
+
+    def to_df(self):
+        bank = pd.DataFrame([list(k) + [v[k] for k in AnnotationBank.HEADER_INFO]
+                                for k, v in self._bank.items()])
+        bank.columns = AnnotationBank.HEADER
+        return bank
 
 def automatic_bank_annotation(bank_path, annotated_paradigms, new_conj_tables):
     bank = AnnotationBank(bank_path, annotated_paradigms)
@@ -111,6 +160,65 @@ def automatic_bank_annotation(bank_path, annotated_paradigms, new_conj_tables):
     outputs.insert(0, OrderedDict((i, x) for i, x in enumerate(map(str.upper, header))))
     return outputs
 
+def bank_cleanup_checker(bank_path, gsheet_info):
+    def fetch_bank():
+        bank = AnnotationBank(bank_path, gsheet_info=gsheet_info, download_bank=True)
+        bank_partial_key = {}
+        for key, info in bank._bank.items():
+            bank_partial_key.setdefault(key[:-1], {}).setdefault(key[-1], []).append(info)
+        return bank, bank_partial_key
+
+    print('\nBeginning cleanup...')
+    fixed = False
+    while not fixed:
+        bank, bank_partial_key = fetch_bank()
+        over_generations = {}
+        for partial_key, diacs in bank_partial_key.items():
+            for diac, infos in diacs.items():
+                if len(infos) > 1:
+                    over_generations[partial_key + (diac,)] = infos
+        
+        _add_check_mark_online(bank, over_generations)
+        if len(over_generations) != 0:
+            input('Inspect CHECK instances in Google Sheets then press Enter to reevaluate:')
+        else:
+            print('Overgeneration: OK')
+            fixed = True
+
+    fixed = False
+    while not fixed:
+        bank, bank_partial_key = fetch_bank()
+        conflicting_annotations = {}
+        bank_mt1_diac = {partial_key: diacs for partial_key, diacs in bank_partial_key.items() if len(diacs) > 1}
+        for partial_key, diacs in bank_mt1_diac.items():
+            if [diac[0]['QC'] for diac in diacs.values()].count('OK') > 1:
+                for diac, infos in diacs.items():
+                    conflicting_annotations[partial_key + (diac,)] = infos[0]
+        
+        _add_check_mark_online(bank, conflicting_annotations)
+        if len(conflicting_annotations) != 0:
+            input('Inspect CHECK instances in Google Sheets then press Enter to reevaluate:')
+        else:
+            print('Conflicting annotations: OK')
+            fixed = True
+
+    bank = AnnotationBank(bank_path, gsheet_info=gsheet_info, download_bank=True)
+    bank._save_bank()
+
+    print('Cleanup completed.\n')
+
+
+def _add_check_mark_online(bank, error_cases):
+    bank_df = bank.to_df()
+
+    bank_df['STATUS'] = ''
+    for i, row in bank_df.iterrows():
+        if (row['SIGNATURE'], row['LEMMA'], row['DIAC']) in error_cases:
+            bank_df.loc[i, 'STATUS'] = 'CHECK'
+
+    bank._upload_gsheet(bank_df)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-gsheet", default='',
@@ -125,11 +233,24 @@ if __name__ == "__main__":
                         type=str, help="Directory in which the annotation banks are.")
     parser.add_argument("-bank_name",  default='',
                         type=str, help="Name of the annotation bank to use.")
+    parser.add_argument("-bank_spreadsheet", default='Paradigm-Banks',
+                        type=str, help="Name of the spreadsheet in which that sheet is.")
     parser.add_argument("-output_name",
                         type=str, help="Name of the file to output the automatically bank-annotated paradigm tables to.")
     parser.add_argument("-output_dir", default='conjugation/paradigm_debugging',
                         type=str, help="Path of the directory to output the paradigm tables to.")
+    parser.add_argument("-mode", default='debugging', choices=['debugging', 'bank_cleanup'],
+                        type=str, help="Path of the directory to output the paradigm tables to.")
     args = parser.parse_args()
+
+    bank_path = os.path.join(args.bank_dir, f"{args.bank_name}.tsv")
+
+    if args.mode == 'bank_cleanup':
+        gsheet_info = {
+            'gsheet_name': bank_path.split('/')[-1].split('.')[0],
+            'spreadsheet': args.bank_spreadsheet}
+        bank_cleanup_checker(bank_path, gsheet_info)
+        sys.exit()
 
     def create_dir_if_does_not_exist(directory):
         outer_dir = directory.split('/')[0]
@@ -160,7 +281,7 @@ if __name__ == "__main__":
     new_conj_tables = pd.read_csv(args.new_conj, delimiter='\t')
     new_conj_tables = new_conj_tables.replace(nan, '', regex=True)
 
-    outputs = automatic_bank_annotation(bank_path=os.path.join(args.bank_dir, f"{args.bank_name}.tsv"),
+    outputs = automatic_bank_annotation(bank_path=bank_path,
                                         annotated_paradigms=annotated_paradigms,
                                         new_conj_tables=new_conj_tables)
 
