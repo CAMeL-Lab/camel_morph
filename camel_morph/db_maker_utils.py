b196ccd756b8ad5962ea2b24b1e3e91f77f36084
Christian Cayralat <christian-cayralat@hotmail.com>
2023-03-07 14:29:08 +0400

    Temporary fix of unused conditions in db_maker_utils

--- camel_morph/db_maker_utils.py
+++ camel_morph/db_maker_utils.py
@@ -258,6 +258,10 @@ def read_morph_specs(config:Dict,
                 LEXICON[lex_type].at[i, 'COND-T'] = ' '.join(cond_t_)
                 LEXICON[lex_type].at[i, 'COND-F'] = ' '.join(cond_f_)
     
+    #FIXME: temporary; should be done by the clean condisitons code
+    LEXICON['concrete']['COND-S'] = LEXICON['concrete']['COND-S'].replace(r'hollow|defective', '', regex=True)
+    LEXICON['concrete']['COND-S'] = LEXICON['concrete']['COND-S'].replace(r' +', ' ', regex=True)
+    
     # Get rid of unused conditions, i.e., use only the conditions which are in the intersection
     # of the collective (concatenated across morph and lexicon sheets) COND-T and COND-S columns
     # Replace space in gloss field by #, and get rid of excess spaces in the condition field.
